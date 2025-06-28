2cf7353653160f099b558ebe6288e2a7b3038570
= <=>
2025-06-28 08:46:27 -0400

    Started Main and Title

--- Title.java
+++ Title.java
@@ -1,22 +1,41 @@
-import java.awt.*;
-import javax.swing.*;
+import java.awt.*;
+
+import javax.swing.*;
+
+
+
+public class Title extends JPanel {
+
+    public Title() {
+
+
+
+        // Create a JFrame instance
+
+        JFrame frame = new JFrame("Garbage Game");
+
+
+
+        // Set default close operation
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(400, 300);
+
+        frame.setVisible(true);
+
+
+
+
+
+        this.setBackground(Color.BLACK);
+
+        frame.add(this);
+
+    }
+
+
+
 
-public class Title extends JPanel {
-    public Title() {
-
-        // Create a JFrame instance
-        JFrame frame = new JFrame("Garbage Game");
-
-        // Set default close operation
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
-        frame.setVisible(true);
-
-
-        this.setBackground(Color.BLACK);
-        frame.add(this);
-    }
-
-
-    }
+    
 }
